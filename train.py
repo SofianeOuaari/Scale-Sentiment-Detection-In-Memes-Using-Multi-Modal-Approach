1b932727d41e8cce563651b840824efcd38d9218
SofianeOuaari <sofiane.ouaari@gmail.com>
2022-01-12 09:17:10 +0100

    Instanciating all models in the train.py script

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import numpy as np
 from preprocess import return_rgb_images_text,preprocess_text,label_encoder,return_all_color_spaces,get_tokenized_padded_text
 from models import Unimodal,Multimodal
 from keras.callbacks import CSVLogger,EarlyStopping
-from utils import change_to_three_sentiment_labels,augment_dataset
+from utils import change_to_three_sentiment_labels,augment_dataset,plot_color_spaces
+import matplotlib.pyplot as plt
 import time
 
 
@@ -45,12 +46,20 @@ if __name__=="__main__":
     #print(unimo.word_embedding_lstm(tokenized_texts,vocab_size,300,5,5,3,is_gpu_available=True).summary())
     
     
+    # LSTM Unimodal
+    w_e_lstm=unimo.word_embedding_lstm(tokenized_texts,vocab_size,300,1,2,3,is_gpu_available=False)
+    w_e_lstm.fit(tokenized_texts,encoded_label,batch_size=32,epochs=15,validation_data=(tokenized_val_text,encoded_label_val))
 
-    '''w_e_lstm=unimo.word_embedding_lstm(tokenized_texts,vocab_size,300,1,2,3,is_gpu_available=False)
-    w_e_lstm.fit(tokenized_texts,encoded_label,batch_size=32,epochs=15,validation_data=(tokenized_val_text,encoded_label_val))'''
 
-    
-    '''multi_modal_lstm_cnn=multi_modal.word_embedding_lstm_cnn(texts,img_arr,vocab_size,300,1,1,1,3,5,(3,3),(2,2),3,False)
+    ## CNN & Resnet Unimodal
+    cnn_model=unimo.image_cnn_model(img_arr,2,2,5,(3,3),(2,2),number_labels=3)
+
+    cnn_model_resnet=unimo.image_resnet_model(img_arr,2,3)
+
+    cnn_model_resnet.fit(img_arr,encoded_label,batch_size=8,epochs=15,validation_data=(img_arr_valid,y_valid))
+
+    # Multimodal Custom CNN+LSTM
+    multi_modal_lstm_cnn=multi_modal.word_embedding_lstm_cnn(texts,img_arr,vocab_size,300,1,1,1,3,5,(3,3),(2,2),3,False)
     print(tokenized_texts.shape)
     encoded_label=encoded_label.reshape(-1,1)
     print(encoded_label.shape)
@@ -61,33 +70,38 @@ if __name__=="__main__":
 
     #assert False
     
-    multi_modal_lstm_cnn.fit([np.array(tokenized_texts),np.array(img_arr)],encoded_label,batch_size=32,epochs=15,validation_data=([tokenized_val_text,img_arr_valid],encoded_label_val))'''
+    multi_modal_lstm_cnn.fit([np.array(tokenized_texts),np.array(img_arr)],encoded_label,batch_size=32,epochs=15,validation_data=([tokenized_val_text,img_arr_valid],encoded_label_val))
     #img_arr=img_arr.astype("float32")/255
-    '''cnn_model=unimo.image_cnn_model(img_arr,2,2,5,(3,3),(2,2),number_labels=3)
-    cnn_model=unimo.image_resnet_model(img_arr,2,3)
-
-    cnn_model.fit(img_arr,encoded_label,batch_size=8,epochs=15,validation_data=(img_arr_valid,y_valid))'''
-
-
-    ### Multi-color channel 
-    '''multi_modal=Multimodal()
     
+    ### Multi-color channel 
+    multi_modal=Multimodal()
+    print("preparing all color spaces")
     hsv_img,lab_img,grey_img,ylcrcb_img=return_all_color_spaces(img_arr)
-    grey_img=np.reshape(grey_img,(-1,224,224,1))
+    plot_color_spaces(hsv_img[5],lab_img[5],grey_img[5],ylcrcb_img[5])
+    hsv_img_val,lab_img_val,grey_img_val,ylcrcb_img_val=return_all_color_spaces(img_arr_valid)
+    print("returning all Color Spaces")
+    #grey_img=np.reshape(grey_img,(-1,224,224,1))
     img_color_spaces=[img_arr,hsv_img,lab_img,grey_img,ylcrcb_img]
-    multi_channel_color_space_cnn=multi_modal.cnn_multiple_color_spaces(img_color_spaces,number_dense_layers_cnn=2,number_cnn_layers=5,number_of_filters=32,filter_size=(3,3),pool_size=(2,2),number_labels=3)
+    multi_channel_color_space_cnn=multi_modal.cnn_multiple_color_spaces(img_color_spaces,number_dense_layers_cnn=2,number_cnn_layers=5,number_of_filters=32,filter_size=(3,3),pool_size=(2,2),number_labels=5)
     es = EarlyStopping(monitor='val_accuracy',patience=5)
     csv_logger = CSVLogger(f'logs_metrics/multi_color_spaces_log_{time.time()}.csv', append=True, separator=',')
-    multi_channel_color_space_cnn.fit([img_arr,hsv_img,lab_img,grey_img,ylcrcb_img],encoded_label,batch_size=8,epochs=15,validation_split=0.25,callbacks=[csv_logger,es])'''
+    multi_channel_color_space_cnn.fit([img_arr,hsv_img,lab_img,grey_img,ylcrcb_img],encoded_label,batch_size=8,epochs=15,validation_data=([img_arr_valid,hsv_img_val,lab_img_val,grey_img_val,ylcrcb_img_val],encoded_label_val),callbacks=[csv_logger,es])
 
 
     ### AutoEncoders
-    multi_modal=Multimodal()
-    print(texts.shape)
+    #  
+    bimodal_autoencoder,bimodal_encoder=multi_modal.text_image_autoencoder(tokenizer,np.array(tokenized_texts),img_arr,vocab_size,300,1,1,1,3,5,(3,3),(2,2),3,False)
 
-    #text_autoencoder,img_autoencoder=multi_modal.text_image_autoencoder(tokenizer,np.array(tokenized_texts),img_arr,vocab_size,300,1,1,1,3,5,(3,3),(2,2),3,False)
-    bimodal_autoencoder=multi_modal.text_image_autoencoder(tokenizer,np.array(tokenized_texts),img_arr,vocab_size,300,1,1,1,3,5,(3,3),(2,2),3,False)
 
+    ### Residual Multimodal Network 
+
+
+    multi_modal.text_image_residual_network(tokenizer,np.array(tokenized_texts),img_arr,encoded_label,vocab_size,300,1,1,1,3,5,(3,3),(2,2),3,False)
+
+    ### MultiEmbedding 
+
+
+    multi_modal.lstm_multi_embedding(tokenizer,np.array(tokenized_texts),encoded_label,vocab_size,3,False)
 
 
 
