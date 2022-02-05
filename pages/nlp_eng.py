5645a1d488622c965df53f740f058620d415f478
SofianeOuaari <sofiane.ouaari@gmail.com>
2022-02-05 13:05:22 +0100

    Adding Text Stemming Operation to the NLP Page

--- pages/nlp_eng.py
+++ pages/nlp_eng.py
@@ -9,7 +9,8 @@ from nlpaug.util.file.download import DownloadUtil
 import nlpaug.augmenter.word as naw
 import nlpaug.flow.sequential as naf
 from googletrans import Translator
-
+import snowballstemmer
+from stop_words import get_stop_words
 
 class FileDownloader(object):
     
@@ -22,6 +23,7 @@ class FileDownloader(object):
 
 	def download(self):
 		b64 = base64.b64encode(self.data.encode()).decode()
+        #timestr = time.strftime("%Y%m%d-%H%M%S")
 		new_filename = "{}_{}_.{}".format(self.filename,time.strftime("%Y%m%d-%H%M%S"),self.file_ext)
 		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Download File</a>'
 		st.markdown(href,unsafe_allow_html=True)
@@ -72,6 +74,7 @@ def augment_with_contextual_embedding(df,emb,action,src_lang,n_aug):
 
         for _ in range(int(n_aug)-1):
             if emb=="Word2vec":
+                #texts=aug_syn.augment(str(t),n=int(n_aug)
                 pass
             elif emb=="Glove":
                 aug_glove = naw.WordEmbsAug(model_type='glove', model_path='gloves/glove.6B.300d.txt',action=action)
@@ -88,6 +91,39 @@ def augment_with_contextual_embedding(df,emb,action,src_lang,n_aug):
     return df.reset_index(drop=True)
 
 
+def return_stemmed_text(df,src_lang,take_off_stopwords=True):
+    lang={'en':'english','fr':'french','it':'italian','es':'spanish','de':'german','hu':'hungarian','cs':'czech','ar':'arabic'}
+
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    stemmer = snowballstemmer.stemmer(lang[src_lang])
+
+    stemmed_text=[]
+    print(df.text.tolist())
+    for t in df.text.tolist():
+        no_punct = ""
+        for char in str(t):
+            if char not in punctuations:
+                no_punct = no_punct + char
+        res = ''.join([i for i in no_punct if not i.isdigit()])
+        res=res.lower()
+        tokens=res.split(" ")
+        stop_words=[]
+        if take_off_stopwords:
+            stop_words = get_stop_words(src_lang)
+        cleaned=""
+        for token in tokens: 
+            if token not in stop_words:
+                cleaned+=stemmer.stemWord(token)+" "
+
+        stemmed_text.append(cleaned)
+    df["stemmed_text"]=stemmed_text
+
+    return df
+            
+    #return cleaned
+
+
+
 
 
 def app():
@@ -100,7 +136,7 @@ def app():
     data = st.file_uploader("Upload a Dataset", type=["csv", "txt"])
     if data is not None:
         df=pd.read_csv(data)
-        st.dataframe(df,width=600,height=500)
+        st.dataframe(df)
         augmentation_type=st.selectbox("Text Augmentation Type",('Synonym-Based','Embedding-Based','Translation'))
 
         if augmentation_type=="Synonym-Based": 
@@ -146,9 +182,21 @@ def app():
                     st.dataframe(df_aug)
                     download = FileDownloader(df_aug.to_csv(),file_ext='csv').download()
         elif augmentation_type=='Embedding-Based':
+            '''DownloadUtil.download_word2vec(dest_dir='.')
+            DownloadUtil.download_glove(model_name='glove.6B', dest_dir='.')
+
+            # download fasttext model
+            DownloadUtil.download_fasttext(model_name='wiki-news-300d-1M', dest_dir='.')'''
+
+            #DownloadUtil.download_fasttext(model_name='wiki.en.vec', dest_dir='.')
+            #aug_glove = naw.WordEmbsAug(model_type='glove', model_path='gloves/glove.6B.300d.txt',action="substitute")
+            #aug_fasttext = naw.WordEmbsAug(model_type='fasttext', model_path='fasttext/wiki.en.vec',action="insert")
+            '''print(len(df.text.iloc[0]))
+            print(len(aug_fasttext.augment(df.text.iloc[0])))'''
             col1,col2,col3,col4=st.columns(4)
             src_lang=None
             with col1:
+                #st.write("Please select the embedding approach to be used")
                 embedding=st.selectbox("Embedding type ",('Word2Vec','Glove','Fasttext'))
             with col2:
                 action=st.selectbox("Augmentation action \n \t",('insert','substitute'))
@@ -174,6 +222,36 @@ def app():
             
 
     st.markdown("<html><body><h3>Text Stemming/Lemming</h3></body></html>",unsafe_allow_html=True)
+
+    st.markdown("Machine Learning models aims to detect key features to distinguish between different classes in case of supervised learning, and in order to simplify the task for the models to focus on more complex pattern learning we have to clean the data from unnecessary information. Within the NLP framework such operations are called **Text Stemming/Lemming**")
+    st.markdown("**_Definition_** Stemming is a useful _normalization_ technique for words. n the context of machine learning based NLP, stemming makes your training data more dense. It reduces the size of the dictionary (number of words used in the corpus) two or three-fold. Having the same corpus, but less input dimensions, ML will work better.")
+
+    data_stem = st.file_uploader("Upload a Dataset For Stemming", type=["csv", "txt"])
+
+    if data_stem is not None:
+        df_stem=pd.read_csv(data_stem)
+        st.dataframe(df_stem)
+
+        st.markdown("Please select the information about the stemming operation")
+
+        col1, col2, col3,col4 = st.columns(4)
+        with col1:
+            pass
+        with col2:
+            src_lang=st.selectbox('Language',('en','fr','it','es','de','hu','cs','ar'))
+
+        with col3:
+            remove_stop_words=st.selectbox('Remove Stopwords',(True,False))
+        with col4:
+            pass
+        
+        stem_button=st.button("Apply Text Stemming & Cleaning")
+        if stem_button:
+            df_stem=return_stemmed_text(df_stem,src_lang,take_off_stopwords=remove_stop_words)
+            st.dataframe(df_stem)
+            download = FileDownloader(df_stem.to_csv(),file_ext='csv').download()
+
+
 
 
 
