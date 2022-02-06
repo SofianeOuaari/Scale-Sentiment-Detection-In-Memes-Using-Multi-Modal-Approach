900e9912f2b502c9ee508c138caa97a1ff3f9cad
SofianeOuaari <sofiane.ouaari@gmail.com>
2022-02-06 13:47:29 +0100

    Adding Model Selection Dropdown Menu + Bar Plotting The Prediction Probabilities

--- pages/ssmml.py
+++ pages/ssmml.py
@@ -1,13 +1,40 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
+import plotly.express as px
 from tomli import load
 from preprocess import get_tokenized_padded_text,image_resize
-from utils import load_keras_tokenizer,load_keras_model,get_glove_embedding_glove
+from utils import load_keras_tokenizer,load_keras_model,get_glove_embedding_glove,get_glove_embedding_fasttext,load_label_encoder
 import pytesseract
 
+
+
+
+def sentiment_prediction_residual(tokenizer,tokenized_val_text,img_arr):
+    residual_network=load_keras_model("residual_network.h5")
+    emb=get_glove_embedding_glove(100,tokenizer,tokenized_val_text.shape[1])
+    X_embedded_val=emb.predict(tokenized_val_text)
+
+    preds=residual_network.predict([np.array(X_embedded_val),np.array([img_arr])])
+    prob=preds[0]
+
+    return prob
+
+
+def sentiment_prediction_multi_emb(tokenizer,tokenized_val_text):
+    multi_emb_network=load_keras_model("multiembedding_network.h5")
+    emb_glove=get_glove_embedding_glove(300,tokenizer,tokenized_val_text.shape[1])
+    emb_fasttext=get_glove_embedding_fasttext(tokenizer,tokenized_val_text.shape[1])
+    X_embedded_val_glove=emb_glove.predict(tokenized_val_text)
+    X_embedded_val_fasttext=emb_fasttext.predict(tokenized_val_text)
+
+    preds=multi_emb_network.predict([np.array(X_embedded_val_glove),np.array(X_embedded_val_fasttext)])
+    prob=preds[0]
+
+    return prob
 
 
 def app():
@@ -33,17 +60,24 @@ def app():
         img_arr=image_resize(img_arr,width=WIDTH,height=HEIGHT)
         print(img_arr.shape)
 
-        residual_network=load_keras_model("residual_network.h5")
 
+        model_type=st.selectbox('Choose the Multi Modal Type',('Residual Network','Multi Embedding Network','Multi Color Spaces'))
+
+        prediction_button=st.button("Apply Inference")
         
+        if prediction_button:
+            if model_type=="Residual Network":
+                prob=sentiment_prediction_residual(tokenizer,tokenized_val_text,img_arr)
+            elif model_type=='Multi Embedding Network':
+                prob=sentiment_prediction_multi_emb(tokenizer,tokenized_val_text)
 
-        emb=get_glove_embedding_glove(100,tokenizer,tokenized_val_text.shape[1])
-        X_embedded_val=emb.predict(tokenized_val_text)
 
-        preds=residual_network.predict([np.array(X_embedded_val),np.array([img_arr])])
-        print(np.argmax(preds))
-        st.write(f"Probabilities Distribution: {preds}")
-        '''print(uploaded_file)
-        print(cv2.imread(uploaded_file))'''
-        '''plt.imshow(np.array(image))
-        plt.show()'''
+            l_e=load_label_encoder("./label_encoder.pickle")
+            df={"label":[],"probability %":[]}
+            for l,p in zip(l_e.classes_,prob):
+                df["label"].append(l)
+                df["probability %"].append(round(p*100,2))
+            
+            df=pd.DataFrame(df)
+            fig=px.bar(df,x="label",y="probability %",color="label",title="Probability Distribution Per Sentiment")
+            st.plotly_chart(fig, use_container_width=True)
